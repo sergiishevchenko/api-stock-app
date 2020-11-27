25362e9ab4ffdaef2071a734195c4ef62b24d831
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-27 15:25:36 +0100

    DEBUG=True

--- stocks/settings.py
+++ stocks/settings.py
@@ -77,8 +77,11 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
