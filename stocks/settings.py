52b2e18b2f39e6715a3a8a2c5febc5123d2648aa
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-27 15:35:57 +0100

    added dj_database_url

--- stocks/settings.py
+++ stocks/settings.py
@@ -66,18 +66,7 @@ WSGI_APPLICATION = 'stocks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stocks_db',
-        'USER': 'postgres',
-        'PASSWORD': 'pythondeveloper2019',
-        'PORT': '5432'
-    }
-}
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
