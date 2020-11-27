cfb15e5669a00fc7d8190d9659654645bcc8b24b
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-27 10:32:34 +0100

    Stock model

--- quotes/views.py
+++ quotes/views.py
@@ -4,14 +4,23 @@ def home(request):
     import requests
     import json
 
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_7c1ceb529b3041e69fcfe82141081a99')
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_7c1ceb529b3041e69fcfe82141081a99')
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as er:
-        api = 'Error...'
+        try:
+            api = json.loads(api_request.content)
+        except Exception as er:
+            api = 'Error...'
 
-    return render(request, 'home.html', {'api': api})
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def add_stock(request):
+    return render(request, 'add_stock.html', {})
